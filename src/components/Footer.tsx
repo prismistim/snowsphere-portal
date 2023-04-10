f228f560712defe8a3847ed83a9fbbb6c1e59fa0
snowsphere <info@snowsphere.net>
2023-04-11 01:34:20 +0900

    feat: プロフィール情報を記したjsonファイルの追加と表示方法等の変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,14 @@ const Footer = () => {
   }
 
   return (
-    <footer className="mt-2 border-t border-neutral-800">
-      <div className="py-2 text-neutral-600">
-        ©{getYear()} snowsphere 
-      </div>
+    <footer className="mt-2 flex justify-between border-t border-neutral-800 py-2">
+      <div className=" text-neutral-600">©{getYear()} snowsphere</div>
+      <a
+        href="/index.json"
+        className="bg-gradient-to-r from-red-400 to-cyan-600 bg-clip-text text-neutral-600 decoration-neutral-500/40  transition duration-500 hover:scale-105 hover:text-transparent"
+      >
+        index.json
+      </a>
     </footer>
   )
 }
