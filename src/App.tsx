3c2274552bdf792c7ee1eca4bc735bd6c38d7f34
snowsphere <info@snowsphere.net>
2023-08-07 02:53:12 +0900

    feat: シェア用のカードを新規で追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,33 @@ import FediverseLinkList from "./components/FediverseLinkList/LinkList"
 import LinkList from "./components/LinkList/LinkList"
 import Footer from "./components/Footer"
 import Toast from "./components/Toast"
-import { ToastContext, useIsShow } from "./hooks/isShowToast"
+import ShareCard from "./components/ShareCard/ShareCard"
 
+import { ToastContext, useIsShow } from "./hooks/isShowToast"
+import { ShareCardContext, useIsShow as useIsShowShareCard  } from "./hooks/isShowShareCard"
 
 const App: React.FC = () => {
-  const context = useIsShow()
+  const toastContext = useIsShow()
+  const shareCardContext = useIsShowShareCard()
 
   return (
-    <ToastContext.Provider value={context}>
-      <div className="content bg-neutral-950 bg-cover min-h-screen">
-        <Layout>
-          <div className="mb-8">
-            <Card />
-          </div>
-          <div className="mb-4 animate-pulse border-b border-neutral-700"></div>
-          <FediverseLinkList></FediverseLinkList>
-          <div className="mb-4 animate-pulse border-b border-neutral-700"></div>
-          <LinkList></LinkList>
-          <Footer></Footer>
-        </Layout>
-        { context.isShow && <Toast></Toast> }
-      </div>
+    <ToastContext.Provider value={toastContext}>
+      <ShareCardContext.Provider value={shareCardContext}>
+        <div className="content bg-neutral-950 bg-cover min-h-screen">
+          { shareCardContext.isShow && <ShareCard></ShareCard> }
+          <Layout>
+            <div className="mb-8">
+              <Card />
+            </div>
+            <div className="mb-4 animate-pulse border-b border-neutral-700"></div>
+            <FediverseLinkList></FediverseLinkList>
+            <div className="mb-4 animate-pulse border-b border-neutral-700"></div>
+            <LinkList></LinkList>
+            <Footer></Footer>
+          </Layout>
+          { toastContext.isShow && <Toast></Toast> }
+        </div>
+      </ShareCardContext.Provider>
     </ToastContext.Provider>
   )
 }
