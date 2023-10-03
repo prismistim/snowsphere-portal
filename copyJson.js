429a39d4bae3d0278dcb367b6d163fabe91d4556
snowsphere <info@snowsphere.net>
2023-10-03 22:30:30 +0900

    コンソール出力の変更

--- copyJson.js
+++ copyJson.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const path = require("path")
 
 try {
-  console.log(process.cwd())
+  console.log(fs.existsSync(path.join(process.cwd(), "src/assets/index.json")))
   fs.copyFileSync(
     path.join(process.cwd(), "src/assets/index.json"),
     path.join(process.cwd(), "public/index.json")
