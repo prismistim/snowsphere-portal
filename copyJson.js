4f39c30e22927d4bd8cefa0c4ea718b9f418400d
snowsphere <info@snowsphere.net>
2023-10-03 22:22:59 +0900

    feat: ファイルコピー処理に検証用のコンソール出力の追加

--- copyJson.js
+++ copyJson.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const path = require("path")
 
 try {
+  console.log(process.cwd())
   fs.copyFileSync(
     path.join(process.cwd(), "src/assets/index.json"),
     path.join(process.cwd(), "public/index.json")
