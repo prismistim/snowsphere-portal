ae5efc18665bce1ee1d08b0f0a9c42541e2c8415
snowsphere <info@snowsphere.net>
2024-02-07 05:47:59 +0900

    feat: public/ に画像追加

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -9,7 +9,7 @@ const Head: React.FC = () => {
       <meta property="og:url" content="https://portal.snowsphere.net/" />
       <meta property="og:title" content="portal.snowsphere.net" />
       <meta property="og:description" content="snowsphere's portal site" />
-      <meta property="og:image" content="https://portal.snowsphere.net/assets/static/rb_icon_me_medium.png" />
+      <meta property="og:image" content="https://portal.snowsphere.net/rb_icon_me_medium.png" />
       <link rel="stylesheet" href="https://use.typekit.net/sox5oza.css"></link>
       <script defer data-domain="portal.snowsphere.net" src="https://tracker.snowsphere.net/js/plausible.js"></script>
     </>
