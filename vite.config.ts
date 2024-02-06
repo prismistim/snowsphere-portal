711520ad2e2135988c286ff1f587a4419837a4e6
snowsphere <info@snowsphere.net>
2024-02-07 03:35:43 +0900

    feat: vercelへの対応を追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import vike from 'vike/plugin'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer(), vike({
-    
-  })],
+  plugins: [react(), ViteImageOptimizer(), vercel(), vike({})],
 })
