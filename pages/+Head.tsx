0d76e3629ca630c265d089fcfb45d5c6ac5116c5
snowsphere <info@snowsphere.net>
2024-02-07 04:59:07 +0900

    feat: head内の定義を追加

---
+++ pages/+Head.tsx
@@ -0,0 +1,20 @@
+const Head: React.FC = () => {
+  return (
+    <>
+      <meta charset="UTF-8" />
+      <link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>portal.snowsphere.net</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@Prismist_M" />
+      <meta name="twitter:creator" content="@Prismist_M" />
+      <meta property="og:url" content="https://portal.snowsphere.net/" />
+      <meta property="og:title" content="portal.snowsphere.net" />
+      <meta property="og:description" content="snowsphere's portal site" />
+      <meta property="og:image" content="https://portal.snowsphere.net/rb_icon_me_medium.png" />
+      <link rel="stylesheet" href="https://use.typekit.net/sox5oza.css"></link>
+    </>
+  )
+}
+
+export default Head
